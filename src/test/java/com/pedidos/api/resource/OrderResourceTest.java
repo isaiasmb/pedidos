28f80deda387fcccf7472d9fdd158dfc958c5dfb
Isaias Morona <isaiasbuscarino@hotmail.com>
2018-01-17 17:50:28 -0200

    Separando responsabilidades nos testes.

--- src/test/java/com/pedidos/api/resource/OrderResourceTest.java
+++ src/test/java/com/pedidos/api/resource/OrderResourceTest.java
@@ -1,13 +1,10 @@
 package com.pedidos.api.resource;
 
-import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.Arrays;
 
 import org.junit.Before;
@@ -16,10 +13,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -37,6 +30,7 @@ import com.pedidos.api.repository.ClientRepository;
 import com.pedidos.api.repository.ItemRepository;
 import com.pedidos.api.repository.OrderRepository;
 import com.pedidos.api.repository.ProductRepository;
+import com.pedidos.api.utils.TestUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = PedidosApiApplication.class)
@@ -45,12 +39,6 @@ import com.pedidos.api.repository.ProductRepository;
 public class OrderResourceTest {
 	
 	final String BASE_PATH = "http://localhost:8080/orders";
-	
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
-	
-	private HttpMessageConverter mappingJackson2HttpMessageConverter;
 	
 	@MockBean
 	private OrderResource orderResource;
@@ -73,16 +61,7 @@ public class OrderResourceTest {
 	private Item item;
 		
 	private MockMvc mvc;
-	
-	@Autowired
-    void setConverters(HttpMessageConverter<?>[] converters) {
-        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
-            .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
-            .findAny()
-            .orElse(null);
-
-        assertNotNull("the JSON message converter must not be null", this.mappingJackson2HttpMessageConverter);
-    }
+	private TestUtils utils;
 	
 	@Before
 	public void setUp() {
@@ -102,6 +81,7 @@ public class OrderResourceTest {
 		orderRepository.save(order);
 		
 		mvc = MockMvcBuilders.standaloneSetup(orderResource).build();
+		utils = new TestUtils();
 	}
 	
 	@Test
@@ -109,8 +89,8 @@ public class OrderResourceTest {
 		Order newOrder = OrderBuilder.oneOrder(client, Arrays.asList(item)).now();
 		
 		mvc.perform(post(BASE_PATH)
-				.content(json(newOrder))
-				.contentType(contentType))
+				.content(utils.json(newOrder))
+				.contentType(utils.contentType))
 		.andExpect(status().isOk());
 	}
 	
@@ -122,28 +102,21 @@ public class OrderResourceTest {
 	@Test
 	public void shouldGetOrder() throws Exception {
 		mvc.perform(get(BASE_PATH + "/" + order.getId())
-				.contentType(contentType))
+				.contentType(utils.contentType))
 		.andExpect(status().isOk());
 	}
 	
 	@Test
 	public void shouldGetAllOrders() throws Exception {
 		mvc.perform(get(BASE_PATH)
-				.contentType(contentType))
+				.contentType(utils.contentType))
 		.andExpect(status().isOk());
 	}
 	
 	@Test
 	public void shouldDeleteOrder() throws Exception {
 		mvc.perform(delete(BASE_PATH + "/" + order.getId())
-				.contentType(contentType))
+				.contentType(utils.contentType))
 		.andExpect(status().isNoContent());
 	}
-	
-	protected String json(Object o) throws IOException {
-        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-        this.mappingJackson2HttpMessageConverter.write(
-                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-        return mockHttpOutputMessage.getBodyAsString();
-    }
 }
